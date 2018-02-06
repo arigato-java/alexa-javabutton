489455261b84e9008b436e9d61478eb0323929e7
Hisanobu Tomari <htomari@mail.ru>
2018-02-07 08:24:12 +0900

    Update Java intent

--- src/java.js
+++ src/java.js
@@ -1,15 +1,46 @@
 'use strict';
 const Alexa=require('alexa-sdk');
+const HelpMessage='例えば、30億回ジャバして、などと言ってください。';
 
 var handlers={
 	'LaunchRequest':function(){
-		this.response.speak('30億のデバイスで走るジャバ')
+		this.response.speak('あなたとジャバ、今すぐダウンロード')
 		this.emit(':responseReady');
 	},
-	'MyIntent':function(){
+	'JavaIntent':function(){
+		var javaCount=0;
+		try{
+			javaCount=this.event.request.intent.slots.Javacount.value;
+		}catch(ignored){}
 		// 呼びかけで「あなジャバ」はプレイヤーが発言している
-		this.response.speak('今すぐダウンロード');
+		var utter=[];
+		if(javaCount && 10<javaCount){
+			utter.push(javaCount+'のデバイスで走るジャバ');
+		}else{
+			if(!javaCount){javaCount=1;}
+			for(var i=0;i<javaCount;i++){
+				utter.push('ジャバ');
+			}
+		}
+		var res=utter.join('、')+'。';
+		this.response.cardRenderer('ジャバボタン',res);
+		this.response.speak(res);
 		this.emit(':responseReady');
+	},
+	'AMAZON.HelpIntent':function(){
+		this.response.speak(HelpMessage);
+		this.emit(':ask');
+	},
+	'AMAZON.CancelIntent':function(){
+		this.response.speak('やめるジャバ');
+		this.emit(':responseReady');
+	},
+	'AMAZON.StopIntent':function(){
+		this.response.speak('コボルのように死んだ言語。ジャバは置き換えられる時期に来ているのか。');
+		this.emit(':responseReady');
+	},
+	'Unhandled':function(){
+		this.emit(':ask',HelpMessage,HelpMessage);
 	}
 };
 exports.handler=function(event,context,callback){
