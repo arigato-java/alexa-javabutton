ae6b65ee4da784029a553c31df83c945e31deb8a
Hisanobu Tomari <htomari@mail.ru>
2018-02-07 21:18:32 +0900

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+SRC_PATH=`realpath \`dirname $0\`/../src`
+
+pushd "$SRC_PATH"
+npm update
+rm -f ../lambda.zip
+zip -9rXq ../lambda.zip java.js node_modules packages.json
+aws --profile lambda lambda update-function-code --function-name AlexaJavabutton --zip-file fileb://../lambda.zip
+#aws --profile lambda lambda update-function-configuration --function-name AlexaJavabutton --handler java.handler
+popd
+
+
