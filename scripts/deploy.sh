9d9190b86eea3796ec1171a132006d51dfb70edf
Hisanobu Tomari <htomari@mail.ru>
2018-02-28 22:46:41 +0900

    Add more samples

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ pushd "$DIST_PATH"
 npm update --production
 rm -f ../lambda.zip
 zip -9rXq ../lambda.zip $DIST_FILES node_modules
-echo aws --profile lambda lambda update-function-code --function-name AlexaJavabutton --zip-file fileb://../lambda.zip
+aws --profile lambda lambda update-function-code --function-name AlexaJavabutton --zip-file fileb://../lambda.zip
 #aws --profile lambda lambda update-function-configuration --function-name AlexaJavabutton --handler java.handler
 popd
 
