5961d6ee433cd47249f904574dc39080bf0544c8
Hisanobu Tomari <htomari@mail.ru>
2018-03-08 21:11:55 +0900

    Update blacklist and replace sequence

--- src/java.js
+++ src/java.js
@@ -9,15 +9,27 @@ const KakugenBlacklist=new Set([
 	"48f26d4e815c74a10ee471c04677123ca41dbd24e3e9cd9f388e5e17fbe395c6",
 	"0eadad98665b6c3d6a177f4f6001c0f1b2fbff3db8a4052c08a1e739d8b6a807",
 	"285d3ccdd9bf477eec1abf8d4f148dd5c17bbd99deaf2fdfe14af54bef0b622e",
-	"988bc15a584a7886520d56e9c1855ce8b90c311793317bf373e3584f717a876c"
+	"988bc15a584a7886520d56e9c1855ce8b90c311793317bf373e3584f717a876c",
+	"34e191626e98231012856dc4b02717d6a273c2a8607f2d371c0b1e3d371bd55a",
+	"e626fa4128497573725a2e991083ec14dcd06b7f8d1382240694e8a4e84a8d09",
+	"51fff1672e731da5c3aa8249fe412062be06a49e7b721ee127dc52470193d90d",
+	"f16312f46ac29fd456b05c66f6a275cefd6338d06d24840a7aaa07dadb0c92d6",
+	"89ec7245128eaf1880fb2efa8ba4e5b16e1ffc884e3219849f2cfd10ce72cb18",
+	"3d9dc3c78cf2af5d7588a14f67c0c774939bcf676b5e22634b9aefa42e5b2606",
+	"a736287e9758abebf3bc38796cb1a0d78b0de143b9962af9ca717d370c1839fc",
+	"a6ed1eaab74c44bf0c2c0fcc4a954fdb4f736bdad3430ee8626c06e9de072624",
+	"9aa5e562a8e79ab030c054ed8619e7d4edf4a8448971ce2e86920ec55e1606ad"
 ]);
 
 function KakugenAmazonify(kakugen){
+	const emojiRegex=require('emoji-regex/es2015/text.js');
+	const emojis=emojiRegex();
 	let amzn1=kakugen.replace(/ダウンロード?/g,'ド');
-	let amzn2=amzn1.replace(/[?？]/g,'。');
+	let amzn2=amzn1.replace(/[?？｜]/g,'。');
 	let amzn3=amzn2.replace(/自殺/g,'ノットジャバ');
 	let amzn4=amzn3.replace(/殺/g,'コロコロ');
-	return amzn4;
+	let amzn5=amzn4.replace(emojis,'');
+	return amzn5;
 }
 
 var handlers={
